b76b6753cafd2fc6ff5579cad18df5c104462a17
Muhammad Arief Huda <ariefhuda434@gmail.com>
2025-12-12 14:49:07 +0700

    ini mau pindah bentar

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -3,10 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TransactionController extends Controller
 {
     public function index(){
-        return view('transaksi');
+        $transaksiData = DB::table('orders')
+    ->join('customers', 'customers.Customer_id', '=', 'orders.Customer_id')
+    ->join('employees', 'employees.Employee_id', '=', 'orders.Employee_id')
+    ->join('tables', 'tables.No_Table', '=', 'customers.No_Table')
+    ->whereNotIn('orders.Order_Status', ['Selesai', 'Lunas', 'Batal']) 
+    ->select([
+        'orders.Order_id',
+        'tables.No_Table as nomor_meja',
+        'customers.Name as nama_customer',
+        'customers.Phone_Number as nomor_hp',
+        'orders.Order_Status as status_order',
+        'tables.status_table as status_meja', 
+        'employees.Name_Employee as nama_pelayan',
+        'orders.Total as total_harga',
+        'orders.created_at',
+    ])
+    ->orderBy('orders.created_at', 'desc')
+    ->get();
+        return view('transaksi',[
+            'transaksiData' => $transaksiData
+
+        ]);
     }
 }

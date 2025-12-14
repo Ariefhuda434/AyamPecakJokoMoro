9c65aa2bd2d0b26654bfe868eda7485a7af9554b
Billranda23 <alubill47@gmail.com>
2025-12-14 13:21:09 +0700

    ini rif

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -16,26 +16,16 @@ class RecipeController extends Controller
    public function index(Request $request, string $slug)
 {
     $menu = Menu::where('slug', $slug)->firstOrFail();
-// dd($menu);
     $resepid = $menu->Recipe_id;
 
     $resepData = DB::table('view_resep_stok')
         ->where('id_resep', $resepid)
         ->first();
-    //jangan ilang bil
-    $Data = DB::table('recipe_pivot')
-        ->join('stocks','stocks.Stock_id','=','recipe_pivot.Stock_id')
-        ->join('recipies','recipies.Recipe_id','=','recipe_pivot.Recipe_id')
-        ->join('menus','menus.Recipe_id','=','recipies.Recipe_id')
-        ->where('menus.Recipe_id',$resepid)
-        ->select([
-            'stocks.Stock_id',
-            'stocks.Name_Stock as nama_stock_resep',
-            'stocks.Unit as Satuan_resep',
-            'recipe_pivot.Quantity as jumlah_stock_resep',
-        ])->get();
-        
-        // dd($resepData);
+
+    $Data = DB::table('view_resep_detail')
+        ->where('id_resep', $resepid)
+        ->get();
+
     $stockData = DB::table('stocks')
         ->select([
             'Stock_id',
@@ -43,7 +33,7 @@ class RecipeController extends Controller
             'Unit as satuan_resep'
         ])
         ->get();
-        // dd($stockData);
+
     return view('recipies', [
         'menu' => $menu,
         'resepData' => $resepData,
@@ -51,6 +41,7 @@ class RecipeController extends Controller
         'Data' => $Data
     ]);
 }
+
 
      public function store(Request $request)
     {
